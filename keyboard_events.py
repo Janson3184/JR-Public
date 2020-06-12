32d11fda2e6c7bfb7212af7e0057bbc88a905ab2
jonathanrice <biohoo25@gmail.com>
2020-06-11 20:25:06 -0700

    keyboard events

--- keyboard_events.py
+++ keyboard_events.py
@@ -5,30 +5,38 @@ import time
 '''
 From: https://pypi.org/project/keyboard/
 '''
+MAX_RECORDS = 81
 
 start = datetime.datetime.now()
 
 string = 'CH000the CH0quick CH0brown CH000fox CH0jumps CH00over CH000the CH00lazy CH000dog'
 
 list = string.split(' ')
-counter = 0
 
-def scan_box(counter):
-    print('Waiting 2 seconds.  Cursors ready...')
-    time.sleep(2)
-    while counter < 81:
+
+def countdown(t):
+    print(f'Waiting {t-1} seconds.  Cursors ready...')
+    for i in reversed(range(1,t)):
+        print(i)
+        time.sleep(1)
+
+def scan_box():
+
+    countdown(4)
+
+    counter = 0
+    while counter < MAX_RECORDS:
         for word in list:
             keyboard.write(word)
             keyboard.press_and_release('return')
             counter += 1
 
-scan_box(counter)
+scan_box()
 
 stop = datetime.datetime.now()
 
-print(f'Elapsed: {stop-start}')
+print(f'Elapsed: {stop-start}\nTime Per Scan: {(stop-start)/MAX_RECORDS}')
 
 '''
-
 
 '''
